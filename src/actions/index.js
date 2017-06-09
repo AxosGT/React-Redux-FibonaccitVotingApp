33bf11fe5c0ffc96016eb9b1597a2d925844267f
AxosGT <chaosxaos@gmail.com>
2017-06-08 18:40:22 -0700

    More Redux/FireBase
    tied more act to firebase

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,22 +2,33 @@ import {fb, logInFBGoogle} from '../myFireBase';
 
 export const LOGINFB = 'LOGINFB'
 export const VOTE ='VOTE'
+export const FBVOTE ='FBVOTE'
 export const PAGELODE = 'PAGELODE'
 export const CHANGEUSER = 'CHANGEUSER'
+export const CREATEROOM = 'CREATEROOM'
 
 export function logInFB(actUser) {
   return {type: LOGINFB, actUser}
 }
 
+
 export function vote(intVote) {
-  return { type: VOTE, intVote}
+  return {type: VOTE, intVote}
+}
+
+
+export function fbVote(userVote){
+  return dispatch =>{
+
+  };
 }
 
 export function pageLoad(){
   return dispatch =>{
     fb.auth().onAuthStateChanged(function(user) {
       if (user) {
-        dispatch(logInFB(fb.auth().currentUser.email));
+        dispatch(logInFB(fb.auth().currentUser.displayName));
+        dispatch(createRoom(fb.auth().currentUser));
       } else {
         logInFBGoogle();
       }
@@ -26,14 +37,27 @@ export function pageLoad(){
 }
 
 export function changeUser(){
-  return dispatch=>{
+  return dispatch =>{
     logInFBGoogle();
     fb.auth().onAuthStateChanged(function(user) {
       if (user) {
-        dispatch(logInFB(fb.auth().currentUser.email));
+        dispatch(logInFB(fb.auth().currentUser.displayName));
+        dispatch(createRoom(fb.auth().currentUser));
       } else {
         dispatch(logInFB("User auth failed"));
       }
     });
+    var test = fb.database().ref().child(fb.auth().currentUser.uid+'');
+    test.on('value', snap =>console.log(snap.val()));
   };
+}
+
+export function createRoom(actUser){
+  //console.log(fb.auth().currentUser.providerData);
+  return dispatch => fb.database().ref(actUser.uid).child(actUser.displayName).set({
+    username: actUser.displayName,
+    uid: actUser.uid,
+    vote: 0
+  });
+
 }
